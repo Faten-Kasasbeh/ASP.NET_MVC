0491a2efe54a1cecff82aa322d686da4e16dec0f
Your Name <you@example.com>
2023-01-31 17:00:07 +0300

    task

---
+++ MVC_2/MVC_2/Controllers/DefaultController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace MVC_2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+
+
+        public ActionResult Index()
+        {
+            showImg();
+            return View();
+        }
+
+       public string showImg()
+        {
+            return "<h1>Download Image</h1> <a href= \"GetImage\"> <img src=\"../images/mvc.png\" alt=\"Girl in a jacket\" width=\"500\" height=\"600\"></a>";
+        }
+
+        public FileResult GetImage()
+        {
+            var filePath = Server.MapPath("~/images/" + "mvc.png");
+            return File(filePath, "image/jpeg", "mvc.png");
+
+
+        }
+        public string contact()
+        {
+            return "<h4>CONTACT US</h4><h1>If You Have Any Query, Please Contact Us</h1> <p>The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. Download Now. </p>  ";
+        }
+        public string About()
+        {
+            return "<h4>About US</h4><h1>If You Have Any Query, Please Contact Us</h1> <p>The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. Download Now. </p>  ";
+        }
+
+    }
+}
---
+++ MVC_2/MVC_2/Controllers/FatenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_2.Controllers
+{
+    public class FatenController : Controller
+    {
+        // GET: Faten
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string name()
+        {
+            return "<h1>Faten Kasasbeh</h1>";
+         }
+        public string Age() {
+            return "29 year";
+        }
+        public string image() {
+            return " <img src=\"../images/faten.jpeg\" alt=\"Girl in a jacket\" width=\"500\" height=\"600\">";
+        }
+        public string adress() {
+            return "Jordan / Irbid"; 
+        }
+    }
+}
