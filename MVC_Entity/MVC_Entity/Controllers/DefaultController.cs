4061570307b6e87a6a72ef03339fc3e73f2409e7
Your Name <you@example.com>
2023-02-01 16:58:48 +0300

    task

---
+++ MVC_Entity/MVC_Entity/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using MVC_Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Entity.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            List<Models.Student> stu = new List<Student>();
+            stu.Add(new Student { ID = 1, Name = "Faten", Major = "Communication Engineering" , Faculity= "Engineering" });
+            stu.Add(new Student { ID = 2, Name = "Mohammad", Major = "CS", Faculity = " IT" });
+            stu.Add(new Student { ID = 3, Name = "Razan", Major = " Mathmatic", Faculity = "Sciences" });
+
+            return View(stu);
+        }
+    }
+}
