18cef57a5a6dc08314fa0c4c4b50802f413e1c4a
Your Name <you@example.com>
2023-02-12 16:31:16 +0300

    tasks

---
+++ MVC_12_2/MVC_12_2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_12_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult sum()
+        {
+            int x = 1;
+            int y = 0;
+            int z = x / y;
+            return Content(z.ToString());
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC_12_2/MVC_12_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_12_2.MvcApplication" Language="C#" %>
